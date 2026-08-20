aafcf4e9710054bb304320e7144fa27a3f0ad375
Michael Svensson <Michael.Svensson@lexicon.se>
2026-08-20 05:42:21 +0200

    Added DTOs

---
+++ MovieAPI/DTOs/MovieDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieAPI.DTOs
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public int Year { get; set; }
+        public string Genre { get; set; } = "";
+        public int Duration { get; set; }
+    }
+}
---
+++ MovieAPI/DTOs/ReviewDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieAPI.DTOs
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public string ReviewerName { get; set; } = "";
+        public string Comment { get; set; } = "";
+        public int Rating { get; set; }
+    }
+}
